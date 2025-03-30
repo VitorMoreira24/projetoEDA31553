78e63e4b9123d3b6475bcf974fde7236b6cd9a50
Vitor Moreira <a31553@alunos.ipca.pt>
2025-03-30 19:00:29 +0100

    aaaa

--- projetoEDA/main.c
+++ projetoEDA/main.c
@@ -1,5 +1,5 @@
 /*****************************************************************//**
- * \file   main.c
+ * \file   main.cc
  * \brief  Ficheiro Main do programa AINDA EM DESENVOLVIMENTO/INCORRETO
  *
  * \author Vitor Moreira 31553
