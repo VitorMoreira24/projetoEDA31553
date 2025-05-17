a39aeb36f988ad55e33f50d41221a4109b08724f
Vitor Moreira <a31553@alunos.ipca.pt>
2025-05-17 15:36:01 +0100

    atualizacao15:35

--- projetoEDA/antenas.c
+++ projetoEDA/antenas.c
@@ -234,7 +234,7 @@ void mostrarMatrizNefastos(Antena* lista_antenas, nefastos* lista_nefastos) {
 				}
 
 				if (ehnefasto) {
-					printf("# ");
+					printf("# ");teste
 				}
 				else {
 					printf(". ");
